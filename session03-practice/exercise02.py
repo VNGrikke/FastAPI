34e25f4850f1b49a9d9545f32527c624489b9f42
VNGrikke <Enlove2005@gmail.com>
2026-07-07 09:57:44 +0700

    session 03

---
+++ session03-practice/exercise02.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+books = [
+    {
+        "id": 1,
+        "title": "Python Basic",
+        "author": "Lê Minh Thu",
+        "category": "programming",
+        "year": 2022,
+        "is_available" : True
+    },
+    {
+        "id": 2,
+        "title": "Web API Design",
+        "author": "Phạm Lan Hồng",
+        "category": "web",
+        "year": 2021,
+        "is_available" : False
+    },
+    {
+        "id": 3,
+        "title": "Database System",
+        "author": "Lê Minh Huyền",
+        "category": "database",
+        "year": 2020,
+        "is_available" : False
+    },
+    {
+        "id": 4,
+        "title": "Clean Code",
+        "author": "Lê Ánh Linh",
+        "category": "programming",
+        "year": 2008,
+        "is_available" : True
+    },
+    {
+        "id": 5,
+        "title": "Computer Network",
+        "author": "Vũ Hồng Vân",
+        "category": "network",
+        "year": 2019,
+        "is_available" : True
+    }
+]
+
+
+@app.get("/books/available")
+def get_books():
+    book_available = []
+    for book in books:
+        if book["is_available"] == True:
+            book_available.append(book)
+    return book_available
+
+@app.get("/books/borrowed")
+def get_books():
+    book_borrowed = []
+    for book in books:
+        if book["is_available"] == False:
+            book_borrowed.append(book)
+    return book_borrowed
+
